2bb1ad98cc37ad56809b04d075b90c370938ef80
scyan <ydd-jd@163.com>
2018-12-11 22:35:52 +0800

    版本号升级

--- dist/createStore.js
+++ dist/createStore.js
@@ -1,59 +1,60 @@
-let diff = require('object-diff')
 import deepClone from 'deep-clone'
-function createStore(config){
-  
-  let models = config.models
-  let store={
-    models: models,
-    views:[],
-    getState: ()=>{
-      let states = {}   
+
+const diff = require('object-diff')
+
+
+function createStore(config) {
+  const models = config.models
+  const store = {
+    models,
+    views: [],
+    getState: () => {
+      const states = {}
       Object.keys(models).map((key) => {
         states[key] = models[key].state
       })
       return states
     },
-    dispatch:{}
+    dispatch: {},
   }
-  let rootState = store.getState();
-  //由事件触发一次所有view的变更
-  function handleChange(){
-    let rootState = store.getState()//这里必须重新取一次state,是因为有些state是纯数字，没法通过引用自动修改
-    store.views.map((viewWrap)=>{
-      let {view,mapStateToData} = viewWrap;
-      
-      let data = mapStateToData(rootState)
-      if(view.triggerChange){
-        let diffData = diff(view.data,data)
+  const rootState = store.getState()
+  // 由事件触发一次所有view的变更
+  function handleChange() {
+    const rootState = store.getState()// 这里必须重新取一次state,是因为有些state是纯数字，没法通过引用自动修改
+    store.views.map((viewWrap) => {
+      const { view, mapStateToData } = viewWrap
+
+      const data = mapStateToData(rootState)
+      if (view.triggerChange) {
+        const diffData = diff(view.data, data)
         view.triggerChange(diffData)
       }
     })
   }
-  
-  //遍历reducers和effects，分别传入state和rootState,并触发数据变化，并都绑到dispatch上
-  Object.keys(models).map((key)=>{
-    let model = models[key];
-    let reducers = model.reducers;
-    let effects = model.effects(store.dispatch);
+
+  // 遍历reducers和effects，分别传入state和rootState,并触发数据变化，并都绑到dispatch上
+  Object.keys(models).map((key) => {
+    const model = models[key]
+    const reducers = model.reducers
+    const effects = model.effects(store.dispatch)
 
     store.dispatch[key] = {}
-    for(let func in reducers){
-      let fn = reducers[func];
-      
-      store.dispatch[key][func] = (payload)=>{
-        model.state = fn(model.state,payload)
-        handleChange();
+    for (const func in reducers) {
+      const fn = reducers[func]
+
+      store.dispatch[key][func] = (payload) => {
+        model.state = fn(model.state, payload)
+        handleChange()
       }
     }
-    for(let func in effects){
-      let fn = effects[func];
-      
-      store.dispatch[key][func] =  (payload) => {
-         fn(payload,rootState)
+    for (const func in effects) {
+      const fn = effects[func]
+
+      store.dispatch[key][func] = (payload) => {
+        fn(payload, rootState)
       }
     }
-    
   })
   return store
 }
-module.exports=createStore
+module.exports = createStore
